64f19681e912ca899bbef71f958760728b8c796b
razanalk <13238holbeertonschool@gmail.com>
2026-06-13 13:37:51 +0000

    Fix Bob case

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -11,4 +11,7 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
-    print("My name is {} {}".format(first_name, last_name))
+    name = first_name if last_name == "" else "{} {}".format(
+        first_name, last_name
+    )
+    print("My name is {}".format(name))
