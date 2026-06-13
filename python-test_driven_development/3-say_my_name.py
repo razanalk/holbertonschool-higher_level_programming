c8dc77c7030fd270ece7dbab48cbdd3e1cde5808
razanalk <13238holbeertonschool@gmail.com>
2026-06-13 14:01:32 +0000

    Fix say_my_name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-"""Module that defines say_my_name"""
+# 3-say_my_name.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Defines a name-printing function."""
 
 
 def say_my_name(first_name, last_name=""):
-    """Prints My name is <first name> <last name>"""
+    """Print a name.
 
+    Args:
+        first_name (str): The first name to print.
+        last_name (str): The last name to print.
+    Raises:
+        TypeError: If either of first_name or last_name are not strings.
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-
-    name = first_name if last_name == "" else "{} {}".format(
-        first_name, last_name
-    )
-    print("My name is {}".format(name))
+    print("My name is {} {}".format(first_name, last_name))
