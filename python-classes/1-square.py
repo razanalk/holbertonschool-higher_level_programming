2cbf436f3993ca510eeeb0a8364b28e935701ad1
razanalk <13238holbeertonschool@gmail.com>
2026-06-14 14:38:25 +0000

    Task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+
+class Square:
+    """Square class"""
+
+    def __init__(self, size):
+        """Initialize square with size"""
+        self.__size = size
