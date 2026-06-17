b38dd12aa708002fa3dbdac2c2160fbd2d368691
razanalk <13238holbeertonschool@gmail.com>
2026-06-17 15:07:03 +0000

    Task 5 Detect instance deletion

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -6,15 +6,18 @@ class Rectangle:
     """Rectangle class."""
 
     def __init__(self, width=0, height=0):
+        """Initialize rectangle."""
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """Retrieve width."""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Set width."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -23,10 +26,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """Retrieve height."""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Set height."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -34,27 +39,32 @@ class Rectangle:
         self.__height = value
 
     def area(self):
+        """Return area."""
         return self.__width * self.__height
 
     def perimeter(self):
+        """Return perimeter."""
         if self.__width == 0 or self.__height == 0:
             return 0
-        return 2 * (self.__width + self.__height)
+        return (self.__width * 2) + (self.__height * 2)
 
     def __str__(self):
+        """Print rectangle using #."""
         if self.__width == 0 or self.__height == 0:
             return ""
 
-        return "\n".join(
-            ["#" * self.__width for i in range(self.__height)]
-        )
+        rows = []
+        for i in range(self.__height):
+            rows.append("#" * self.__width)
+        return "\n".join(rows)
 
     def __repr__(self):
+        """Return string representation for eval()."""
         return "Rectangle({}, {})".format(
             self.__width,
             self.__height
-        
         )
 
     def __del__(self):
-    print("Bye rectangle...")        
+        """Called when instance is deleted."""
+        print("Bye rectangle...")
