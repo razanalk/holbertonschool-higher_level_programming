6bec73c121bebab68dce3660807ffb6c99c7746e
razanalk <13238holbeertonschool@gmail.com>
2026-06-17 14:55:27 +0000

    Task 3 string representation

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""Module that defines a Rectangle class."""
+
+
+class Rectangle:
+    """Rectangle class."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize rectangle."""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrieve width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set width."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieve height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set height."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return rectangle area."""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Return rectangle perimeter."""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Return printable rectangle using #."""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
+        return "\n".join(
+            ["#" * self.__width for i in range(self.__height)]
+        )
