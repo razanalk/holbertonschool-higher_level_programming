79ba7d02ad2ca04b117552223ed3a01c55ba7130
razanalk <13238holbeertonschool@gmail.com>
2026-06-22 14:40:25 +0000

    Task 0: Lookup

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module that returns available attributes and methods."""
+
+
+def lookup(obj):
+    """Return list of attributes and methods of an object."""
+    return dir(obj)
---
+++ python-inheritance/README.md
@@ -0,0 +1,11 @@
+Python - Inheritance
+
+This project covers:
+- Inheritance
+- Superclass and subclass
+- isinstance()
+- issubclass()
+- type()
+- super()
+
+Author: Razan
