7517361b5f24cdaeb880fb1c1540159e8116241c
razanalk <13238holbeertonschool@gmail.com>
2026-06-30 14:23:22 +0000

    Add JSON string to object conversion

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for converting JSON string to Python object."""
+
+import json
+
+
+def from_json_string(my_str):
+    """Return a Python object represented by a JSON string."""
+    return json.loads(my_str)
