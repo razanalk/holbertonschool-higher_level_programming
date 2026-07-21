2ee16add73cee4e9888e2e723208b80b2780ee12
razanalk <13238holbeertonschool@gmail.com>
2026-07-21 08:48:45 +0000

    Task 10: Genre ID by show

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,9 @@
+-- Lists all shows with their genre IDs
+
+SELECT
+    tv_shows.title,
+    tv_show_genres.genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
