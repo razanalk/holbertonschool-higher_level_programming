ea2695300dbebf73d3aed6b61c76c302a9bc8e96
razanalk <13238holbeertonschool@gmail.com>
2026-07-21 13:14:18 +0000

    Task 1: Filter states

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Lists states starting with N.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cur = conn.cursor()
+
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+    )
+
+    for row in cur.fetchall():
+        print(row)
+
+    cur.close()
+    conn.close()
