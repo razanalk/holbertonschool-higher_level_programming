cb261b9d82eb90c613c0ca850b0722cf504064e4
razanalk <13238holbeertonschool@gmail.com>
2026-07-22 12:51:32 +0000

    Task 14: Cities in state

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Lists all cities by state."""
+
+import sys
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from model_state import Base, State
+from model_city import City
+
+
+if __name__ == "__main__":
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    cities = (
+        session.query(City, State)
+        .filter(City.state_id == State.id)
+        .order_by(City.id)
+        .all()
+    )
+
+    for city, state in cities:
+        print("{}: ({}) {}".format(
+            state.name,
+            city.id,
+            city.name
+        ))
+
+    session.close()
