3fac479e5ea49b59fe4490b718314c65fc9b516d
razanalk <13238holbeertonschool@gmail.com>
2026-07-22 11:53:39 +0000

    Task 6: First state model

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines the State class."""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Represents the states table."""
+
+    __tablename__ = "states"
+
+    id = Column(Integer,
+                primary_key=True,
+                nullable=False,
+                autoincrement=True)
+
+    name = Column(String(128),
+                  nullable=False)
