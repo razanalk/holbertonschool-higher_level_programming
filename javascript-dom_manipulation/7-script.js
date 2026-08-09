0ac5b0a3687619dcaff550900ba3f3fd6f7f911e
razanalk <13238holbeertonschool@gmail.com>
2026-08-09 18:05:37 +0200

    Add Star Wars movies fetch

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,11 @@
+const movieList = document.querySelector('#list_movies');
+
+fetch('https://swapi-api.hbtn.io/api/films/?format=json')
+  .then(response => response.json())
+  .then(data => {
+    data.results.forEach(movie => {
+      const listItem = document.createElement('li');
+      listItem.textContent = movie.title;
+      movieList.appendChild(listItem);
+    });
+  });
