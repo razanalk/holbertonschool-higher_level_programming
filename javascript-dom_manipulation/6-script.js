bc442fcacb258cf53102b59c23c1254a99390518
razanalk <13238holbeertonschool@gmail.com>
2026-08-09 18:03:37 +0200

    Add Star Wars character fetch

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const character = document.querySelector('#character');
+
+  fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
+    .then(response => response.json())
+    .then(data => {
+      character.textContent = data.name;
+    });
+});
